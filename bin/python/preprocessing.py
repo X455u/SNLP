82e5893fc65d401bd123fb6d2dbbb954f3c270e3
Kasper Hellström <kassu7@gmail.com>
2015-04-20 21:10:53 +0300

    Fix to include åäö in term document matrix

--- bin/python/preprocessing.py
+++ bin/python/preprocessing.py
@@ -1,69 +1,86 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import csv
+import re
 import sys
+import unicodecsv
 import textmining
 
-from nltk import word_tokenize
+from nltk import download, word_tokenize
+
+
+def tokenize(document):
+    """
+    A tokenizer for the Python Textmining Package that works for unicode strings
+    and doesn't replace non-ASCII characters (such as 'åäö') with spaces.
+    """
+    document = document.lower()
+    document = re.sub(u'[^\w]|[0-9]', u' ', document, flags=re.UNICODE)
+    return document.strip().split()
+
 
 if len(sys.argv) != 3:
-	print("Usage: python preprocessing.py <inputfile> <outputfile>")
+    print('Usage: python preprocessing.py <inputfile> <outputfile>')
+
+download('punkt')
 
 
-# opening the data file
-f = open(sys.argv[1])
-csv_f = csv.reader(f)
+input_file = sys.argv[1]
 
-path = sys.argv[2]
-
+output_file = sys.argv[2]
 # Text data
 desc = {}
 # Tokens of the text data
 desc_tokens = {}
+# Term document matrix
+tdm = textmining.TermDocumentMatrix(tokenizer=tokenize)
 
-tdm = textmining.TermDocumentMatrix()
 
-idx = -1
+# Open the data file for reading
+with open(input_file) as f:
+    csv_f = unicodecsv.reader(f, encoding='utf-8')
 
-# Read the file and tokenize
-for row in csv_f:
-  idx += 1
+    # Read the file and tokenize
+    for idx, row in enumerate(csv_f):
 
-  if idx == 0:
-    continue
+        if idx == 0:
+            continue
 
-  # Dsecription column
-  text = row[13]
+        # Select the description column
+        text = row[13]
 
-  if text != "":
-    desc[idx] = text
-    print(text)
-    desc_tokens[idx] = word_tokenize(text.decode('utf-8').lower())
-    tdm.add_doc(text)
+        if text != '':
+            desc[idx] = text
+            desc_tokens[idx] = word_tokenize(text, language='finnish')
+            tdm.add_doc(text)
 
-# cutoff =1, so all of the words
-tdm.write_csv(path, cutoff=1)
+
+# Write term document matrix to output file
+with open(output_file, 'w') as f:
+    w = unicodecsv.writer(f, encoding='utf-8')
+    # cutoff = 1, also include words that only appear in a single document
+    for row in tdm.rows(cutoff=1):
+        w.writerow(row)
 
 
 # Removing non-alphabetic tokens
 for i in desc_tokens.keys():
-  tokens = desc_tokens[i]
+    tokens = desc_tokens[i]
 
 
 # TODO: check/print tokens with non-alpthabetic chars
 
 
 # # TEST!!!!!
-# import re
 # a = {0:"kissa", 1:"koira", 2:".", 3:"kala", 4:"-"}
 # for i in a:
-#   print(a[i])
+#     print(a[i])
 
-#   m = re.search(r"\W+", a[i])
+#     m = re.search(r"\W+", a[i])
 
 # ----------------------------
 
 for i in desc.keys():
-  print(desc[i])
-  print(desc_tokens[i])
-  print("---")
+    print(desc[i])
+    print(desc_tokens[i])
+    print("---")
