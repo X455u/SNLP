bf5c2e2b241a8eff3b4ff9ec044ddb3d7f14b30e
Kasper Hellström <kassu7@gmail.com>
2015-05-04 22:56:50 +0300

    Changed path for input file in readdata

--- bin/matlab/readdata.m
+++ bin/matlab/readdata.m
@@ -1,6 +1,6 @@
 %% Testing reading data
 
-filename = '~/data/snlp/tdm.csv';
+filename = '../../data/tdm.csv';
 delimiterIn = ',';
 headerlinesIn = 1;
 A = importdata(filename,delimiterIn,headerlinesIn);
