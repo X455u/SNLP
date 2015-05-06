6e1ce4cb6502643f9cff9244aa56dfa0f852c5a8
sin <sini.pessala@gmail.com>
2015-05-06 13:20:54 +0300

    preprocessing creates term-document matrices from all of the text fields

--- bin/python/preprocessing.py
+++ bin/python/preprocessing.py
@@ -7,6 +7,7 @@ import unicodecsv
 import textmining
 import nltk
 import ConfigParser
+import json
 # from nltk import download, word_tokenize
 
 
@@ -42,16 +43,25 @@ conf_file = sys.argv[1]
 config = ConfigParser.ConfigParser()
 config.read(conf_file)
 
+# input and output files
 data_file = config.get('data', 'lemmatized')
 tdm_file = config.get('data', 'termdocumentmatrix')
+
+# amount of text fields in the document
+textcolumns = json.loads(config.get('data', 'textdataidx'))
+
+# initializing term document matrices
+tdms = []
+
+for i in range(len(textcolumns)):
+    tdms.append(textmining.TermDocumentMatrix(tokenizer=tokenize))
+    # tdms.append(textmining.TermDocumentMatrix())
 
 
 # Text data
 desc = {}
 # Tokens of the text data
 desc_tokens = {}
-# Term document matrix
-tdm = textmining.TermDocumentMatrix(tokenizer=tokenize)
 
 
 # Open the data file for reading
@@ -64,31 +74,38 @@ with open(data_file) as f:
         if idx == 0:
             continue
 
-        # Select the description column
-        text = row[13]
+        # assuming that the last columns of the data file contain the lemmatized texts
+        for i in range(len(textcolumns)):
+            text = row[len(row)-len(textcolumns)+i]
+            if text != '':
+                desc[idx] = text
+                tdms[i].add_doc(text)
+                # print(text.encode('utf-8'))
 
-        if text != '':
-            desc[idx] = text
-            # tokens = word_tokenize(text, language='finnish')
-            tokens = tokenize(text)
-            real_tokens = remove_stopwords(tokens)
-            desc_tokens[idx] = tokens
-            tdm.add_doc(" ".join(real_tokens))
+                # tokens = word_tokenize(text, language='finnish')
+                
+                # tokens = tokenize(text)
+                # real_tokens = remove_stopwords(tokens)
+                # desc_tokens[idx] = tokens
+                # tdms[i].add_doc(" ".join(real_tokens))
 
-            # print_utf8_list(tokens)
-            # print_utf8_list(real_tokens)
-            # print('---')
-
-
-# TODO: perform lemmatization
+                # print_utf8_list(tokens)
+                # print_utf8_list(real_tokens)
+                # print('---')
 
 
+# Splitting the tdm file path for creating tmd files for all the text columns
+m = re.match( r'(.*)(\..*)', tdm_file)
+tdm_beg =  m.group(1)
+tdm_end = m.group(2)
 
+# Write term document matrices to output files
+for i in range(len(textcolumns)):
+    path = tdm_beg + str(i+1) + tdm_end
 
-# Write term document matrix to output file
-with open(tdm_file, 'w') as f:
-    w = unicodecsv.writer(f, encoding='utf-8')
-    # cutoff = 1, also include words that only appear in a single document
-    for row in tdm.rows(cutoff=1):
-        w.writerow(row)
+    with open(path, 'w+') as f:
+        w = unicodecsv.writer(f, encoding='utf-8')
+        # cutoff = 1, also include words that only appear in a single document
+        for row in tdms[i].rows(cutoff=1):
+            w.writerow(row)
 
