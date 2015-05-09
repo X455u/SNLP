fe28b51ca0dee52b250ab571741aead360b7af08
Kasper Hellström <kassu7@gmail.com>
2015-05-09 15:40:12 +0300

    Changes to readdata.m

--- bin/matlab/readdata.m
+++ bin/matlab/readdata.m
@@ -1,6 +1,16 @@
-%% Testing reading data
+%% Read the data from files
 
-filename = '../../data/tdm.csv';
+tdmfiles = {'../../data/tdm1.csv'; '../../data/tdm2.csv'};
+classfiles = {'../../data/classes1.csv'; '../../data/classes2.csv'};
+
 delimiterIn = ',';
 headerlinesIn = 1;
-A = importdata(filename,delimiterIn,headerlinesIn);
+
+TDMS = {};
+for i = 1:size(tdmfiles, 1),
+   TDMS{i} = importdata(tdmfiles{i}, delimiterIn, headerlinesIn);
+end
+CLASSES = {};
+for i = 1:size(classfiles, 1),
+   CLASSES{i} = importdata(classfiles{i}, delimiterIn, headerlinesIn);
+end
