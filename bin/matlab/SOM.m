6914eab59ab53e38ae69ec100c336d5cd5084f4d
unknown <opalonen@U909-POMTB27.org.aalto.fi>
2015-05-16 15:04:11 +0300

    Added SOM hits plotting

--- bin/matlab/SOM.m
+++ bin/matlab/SOM.m
@@ -1,15 +1,21 @@
-%% Reading the data
+% Reading the data
 filename = 'data1/tdm1.csv';
 delimiterIn = ',';
 headerlinesIn = 1;
 A = importdata(filename,delimiterIn,headerlinesIn);
 
 
-%% SOM
+% SOM
 x = A.data';
-net = selforgmap([6 7]);
-net = train(net,x);
+net = selforgmap([6 7]); % 6 x 7 SOM
+net = train(net,x); % training neural network
 view(net)
 y = net(x);
-classes = vec2ind(y);
+classes = vec2ind(y); 
+
+% Plot SOM sample hits
+plotsomhits(net,x)
+
+% Plot SOM neighbor distances
+plotsomnd(net)
 
